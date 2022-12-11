47bdfcc2233c3bb934bee4721fbed80b03c578ab
Erwin Hessing <ejhessing@gmail.com>
2022-12-11 14:09:47 +1300

    fix darkmode toggle

--- modules/components/ThemeToggle/index.tsx
+++ modules/components/ThemeToggle/index.tsx
@@ -1,3 +1,5 @@
+import { Switch } from "@headlessui/react";
+import cn from "classnames";
 import { useEffect, useState } from "react";
 
 export default function ThemeToggle() {
@@ -32,18 +34,65 @@ export default function ThemeToggle() {
     }
   };
   return (
-    <div className="flex items-center">
-      <input
-        id="default-checkbox"
-        type="checkbox"
-        value=""
+    <div className="flex mx-4 space-x-4 my-3">
+      <Switch
         checked={darkMode}
-        onClick={handleToggle}
-        className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-      />
-      <label className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-        Dark Mode
-      </label>
+        onChange={handleToggle}
+        className={cn(
+          darkMode ? "bg-red-500 border-2 border-slate-50" : "bg-gray-200",
+          "relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-slate-500 focus:ring-offset-2"
+        )}
+      >
+        <span className="sr-only">Use setting</span>
+        <span
+          className={cn(
+            darkMode ? "translate-x-5" : "translate-x-0",
+            "pointer-events-none relative inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out"
+          )}
+        >
+          <span
+            className={cn(
+              darkMode
+                ? "opacity-0 ease-out duration-100"
+                : "opacity-100 ease-in duration-200",
+              "absolute inset-0 flex h-full w-full items-center justify-center transition-opacity"
+            )}
+            aria-hidden="true"
+          >
+            <svg
+              className="h-3 w-3 text-gray-400"
+              fill="none"
+              viewBox="0 0 12 12"
+            >
+              <path
+                d="M4 8l2-2m0 0l2-2M6 6L4 4m2 2l2 2"
+                stroke="currentColor"
+                strokeWidth={2}
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </span>
+          <span
+            className={cn(
+              darkMode
+                ? "opacity-100 ease-in duration-200"
+                : "opacity-0 ease-out duration-100",
+              "absolute inset-0 flex h-full w-full items-center justify-center transition-opacity"
+            )}
+            aria-hidden="true"
+          >
+            <svg
+              className="h-3 w-3 text-slate-600"
+              fill="currentColor"
+              viewBox="0 0 12 12"
+            >
+              <path d="M3.707 5.293a1 1 0 00-1.414 1.414l1.414-1.414zM5 8l-.707.707a1 1 0 001.414 0L5 8zm4.707-3.293a1 1 0 00-1.414-1.414l1.414 1.414zm-7.414 2l2 2 1.414-1.414-2-2-1.414 1.414zm3.414 2l4-4-1.414-1.414-4 4 1.414 1.414z" />
+            </svg>
+          </span>
+        </span>
+      </Switch>
+      <div>Dark Mode</div>
     </div>
   );
 }
